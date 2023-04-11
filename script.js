d993ff8fc1a50815ea2d3ca0349fb502fec9e4fd
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-11 22:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    event.preventDefault()
+    let firstname = document.getElementById("form1").elements[0].value;
+    let lastname = document.getElementById("form1").elements[1].value;
+    alert(firstname + " " + lastname);
 }
